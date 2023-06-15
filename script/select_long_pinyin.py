6937d052f8261c7ce208a1f2c925919eb1ab703f
frankrx41 <frankrx41@gmail.com>
2023-06-16 01:44:42 +0800

    Update select_long_pinyin.py to select length >= 6

--- script/select_long_pinyin.py
+++ script/select_long_pinyin.py
@@ -9,21 +9,24 @@ sqldb = sqlite3.connect(db_path)
 cursor = sqldb.cursor()
 
 cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-cursor.execute("SELECT key FROM pinyin WHERE LENGTH(value) >= 5;")
-# cursor.execute("SELECT key FROM pinyin WHERE LENGTH(value) >= 5 LIMIT 2;")
+cursor.execute("SELECT key FROM pinyin WHERE LENGTH(value) >= 6;")
+# cursor.execute("SELECT key FROM pinyin WHERE LENGTH(value) >= 6 LIMIT 2;")
 
 source_file = open(file_path, 'w')
 
 print(
-"; Store pinyin in db that LENGTH(value) >= 5\n; LENGTH(value) >= 5\n; Generate by " + os.path.basename(__file__) , file=source_file
+"; Store pinyin in db that LENGTH(value) >= 6\n; LENGTH(value) >= 6\n; Generate by " + os.path.basename(__file__) , file=source_file
 )
 
 results = cursor.fetchall() # get all the results as a list of tuples
 pinyin_list = {}
+index = 1
 for result in results:
     key = result[0]
     if key and not key in pinyin_list:
+        # print(str(index) + "\t" + key, file=source_file)
         print(key, file=source_file)
+        index += 1
         pinyin_list[key] = 1
 
 source_file.close()
